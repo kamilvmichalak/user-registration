5a1618555d73fc163d61a5073aa2a9806651c77b
kamilvmichalak <kamilvmichalak@gmail.com>
2024-09-26 19:10:53 +0200

    AppUserService and StudentRepository added

---
+++ src/main/java/com/example/kamil/userregistration/appuser/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.kamil.userregistration.appuser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface StudentRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
