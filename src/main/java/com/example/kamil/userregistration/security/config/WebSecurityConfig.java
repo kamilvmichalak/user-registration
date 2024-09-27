ad9d25315d4d50e34f0a6d2a1068ceee4a8b16bf
kamilvmichalak <kamilvmichalak@gmail.com>
2024-09-27 13:43:56 +0200

    Spring Security

---
+++ src/main/java/com/example/kamil/userregistration/security/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.kamil.userregistration.security.config;
+
+import com.example.kamil.userregistration.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disabling CSRF protection
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v*/registration/**").permitAll() // Everyone can access
+                        .anyRequest().authenticated() // All other requests require authentication
+                )
+                .formLogin(form -> form // Customizing form login
+                        .loginPage("/login") // Specify custom login page
+                        .permitAll() // Allow everyone to see the login page
+                );
+
+        return http.build();
+    }
+
+    // Bean for AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+
+}
