24ad8e11afc62042b55a7ac74ac3f7235d27c6db
kamilvmichalak <kamilvmichalak@gmail.com>
2024-09-29 17:39:34 +0200

    ConfirmationToken done

--- src/main/java/com/example/kamil/userregistration/appuser/AppUserService.java
+++ src/main/java/com/example/kamil/userregistration/appuser/AppUserService.java
@@ -1,6 +1,7 @@
 package com.example.kamil.userregistration.appuser;
 
 import com.example.kamil.userregistration.registration.token.ConfirmationToken;
+import com.example.kamil.userregistration.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,7 @@ public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -37,14 +39,20 @@ public class AppUserService implements UserDetailsService {
 
         appUserRepository.save(appUser);
 
-        //TODO: Send confirmation token
         String token = UUID.randomUUID().toString();
         ConfirmationToken confirmationToken = new ConfirmationToken(
                 token,
                 LocalDateTime.now(),
-                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
                 appUser
         );
-        return "it works";
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        //TODO: SEND EMAIL
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
--- src/main/java/com/example/kamil/userregistration/registration/RegistrationService.java
+++ src/main/java/com/example/kamil/userregistration/registration/RegistrationService.java
@@ -3,14 +3,19 @@ package com.example.kamil.userregistration.registration;
 import com.example.kamil.userregistration.appuser.AppUser;
 import com.example.kamil.userregistration.appuser.AppUserRole;
 import com.example.kamil.userregistration.appuser.AppUserService;
+import com.example.kamil.userregistration.registration.token.ConfirmationToken;
+import com.example.kamil.userregistration.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
 
     private final AppUserService appUserService;
+    private final ConfirmationTokenService confirmationTokenService;
     private EmailValidator emailValidator;
 
     public String register(RegistrationRequest request) {
@@ -27,5 +32,22 @@ public class RegistrationService {
                         AppUserRole.USER
                 )
         );
+    }
+
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService.getToken(token).orElseThrow(() -> new IllegalStateException("Token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        appUserService.enableAppUser(confirmationToken.getAppUser().getEmail());
+        return "confirmed";
     }
 }
